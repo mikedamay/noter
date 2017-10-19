3a092479932a55217c9dfc006119d68d83603096
mikedamay <mikemay@blueyonder.co.uk>
2017-10-19 14:59:02 +0100

    adding tag and user

---
+++ noter/Entities/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace noter.Entities
+{
+    public class Tag
+    {
+        public long Id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [MaxLength(80)]
+        public string ShortDescription { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ noter/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace noter.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; }
+
+        
+        
+    }
+}
