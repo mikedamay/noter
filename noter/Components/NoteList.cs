61994cdd1ebf7ba24603c1f0e023f667748a77c5
mikedamay <mikemay@blueyonder.co.uk>
2017-11-06 13:37:03 +0000

    added note list as sidebar

---
+++ noter/Components/NoteList.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using noter.Services;
+namespace noter.Components
+{
+    public class NoteList : ViewComponent
+    {
+        private INoteManager _noteManager;
+        public NoteList( INoteManager noteManager)
+        {
+            this._noteManager = noteManager;
+           
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var list = await _noteManager.ListNotes();
+            return View(list);
+        }
+        
+    }
+}
