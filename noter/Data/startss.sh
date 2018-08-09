f6b35f359c08e7844a08243b27fd50141fcc2f53
mikedamay <mikemay@blueyonder.co.uk>
2018-08-09 14:58:11 +0100

    fixed problem with migration

--- noter/Data/startss.sh
+++ noter/Data/startss.sh
@@ -1,1 +1,8 @@
+# Mike May - August 2018
+# despite its name this script will run a completely new sql server instance
+# which will need to be populated with noter migrations.
+#
+# Look round for an existing instance using "docker ps -a" for one with the
+# name "sql1" and then do "docker start sql1"
+
 docker run -d --name sql1 -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=M1cromus' -e 'MSSQL_PID=Developer' -p 1401:1433 microsoft/mssql-server-linux:2017-latest
